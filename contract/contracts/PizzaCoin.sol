7c21266a7d3bba40adac28bf445305fb7a1991e3
Victor Hugo Brito Fernandes <victorhbfernandes@gmail.com>
2021-07-24 16:09:19 -0300

    feat: nft mint new pizzacoin and new IPizzaCoin interface

--- contract/contracts/PizzaCoin.sol
+++ contract/contracts/PizzaCoin.sol
@@ -4,6 +4,16 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
+interface IPizzaCoin {
+    function mint(address to, uint256 amount) external;
+    function balanceOf(address addr) external view returns (uint256);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function totalSupply() external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+}
+
 contract PizzaCoin is ERC20, ERC20Burnable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
