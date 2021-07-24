0343566d6e690f100284b08bf65f820594d64b0f
denis.policastro <denis.policastro@gmail.com>
2021-07-24 15:33:19 -0300

    fix

--- contract/test/nft-test.ts
+++ contract/test/nft-test.ts
@@ -2,9 +2,8 @@ import { ethers } from "hardhat";
 import { Contract, ContractFactory, Signer } from "ethers";
 import { expect } from "chai";
 
-
-const pizzaObj = 
-  {
+const pizzaObj =
+{
   "dough": "thin",
   "topping": [
     "pepperoni",
@@ -16,14 +15,12 @@ const pizzaObj =
   ]
 };
 
-
-describe("PizzaNFT", function() {
+describe("PizzaNFT", function () {
   let accounts: Signer[];
   let contract: Contract;
   let ownerAddress: String;
-  let b64Pizza = btoa(JSON.stringify(pizzaObj));
+  let b64Pizza = Buffer.from(JSON.stringify(pizzaObj)).toString('base64')
   let baseURI = "https://pizzatoken.pizza/"
-
 
   beforeEach(async function () {
     accounts = await ethers.getSigners();
@@ -51,8 +48,8 @@ describe("PizzaNFT", function() {
 
   it("Should decode to a valid pizza object", async function () {
     await contract.safeMint(accounts[1].getAddress(), b64Pizza);
-    let TokenURI : string = await contract.tokenURI(0);
-    expect(atob(TokenURI.replace(baseURI, ''))).to.be.equal(JSON.stringify(pizzaObj));
+    let TokenURI: string = await contract.tokenURI(0);
+    expect(Buffer.from(TokenURI.replace(baseURI, ''), 'base64').toString()).to.be.equal(JSON.stringify(pizzaObj));
   });
 
   it("Should return the owner of a token", async function () {
@@ -66,13 +63,11 @@ describe("PizzaNFT", function() {
     await contract.connect(accounts[1]).approve(ownerAddress, 0);
     await contract.burn(0);
     expect(contract.ownerOf(0)).to.be.revertedWith("ERC721: owner query for nonexistent token");
-
   });
 
   it("Should not allow transfer from unnaproved sender", async function () {
     await contract.safeMint(accounts[1].getAddress(), b64Pizza);
-    expect(contract.connect(accounts[2]).transferFrom(accounts[1].getAddress(),accounts[0].getAddress(), 0)).to.be.revertedWith('ERC721: transfer caller is not owner nor approved')
-
+    expect(contract.connect(accounts[2]).transferFrom(accounts[1].getAddress(), accounts[0].getAddress(), 0)).to.be.revertedWith('ERC721: transfer caller is not owner nor approved')
   });
 
 });
