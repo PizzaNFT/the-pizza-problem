1bc9bdf526fadc500aa7aced86699957cec4b368
denis.policastro <denis.policastro@gmail.com>
2021-07-31 17:13:56 -0300

    refactor: improve contract

--- dapp-ui/src/app/services/pizzaBallot.service.ts
+++ dapp-ui/src/app/services/pizzaBallot.service.ts
@@ -9,21 +9,22 @@ import { GlobalService } from './global.service';
 export class PizzaBallotService {
   provider: ethers.providers.Provider
   erc721Address: string
+  contract: ethers.Contract
 
   constructor(private globalService: GlobalService) {
     this.provider = ethers.getDefaultProvider("ropsten", {})
     this.erc721Address = "0x62f997457064bdB83AbD2cFC7f01202A60F12E38"
+    this.contract = new ethers.Contract(this.erc721Address, PizzaToken.abi, this.provider)
   }
 
   async listProposals() {
     console.log("Before")
     const signer = await this.globalService.getSigner().toPromise() as ethers.Signer
     console.log("Signer:", signer)
-    let contract = new ethers.Contract(this.erc721Address, PizzaToken.abi, this.provider)
 
-    contract = await contract.connect(signer)
+    this.contract = await this.contract.connect(signer)
     try {
-      const data = await contract.proposals()
+      const data = await this.contract.proposals()
       console.log('data: ', data)
     } catch (err) {
       console.log("Error: ", err)
@@ -31,11 +32,9 @@ export class PizzaBallotService {
   }
 
   async vote(signer: ethers.Signer) {
-    let contract = new ethers.Contract(this.erc721Address, PizzaToken.abi, this.provider)
-
-    contract = await contract.connect(signer)
+    this.contract = await this.contract.connect(signer)
     try {
-      const data = await contract.vote(1)
+      const data = await this.contract.vote(1)
       console.log('data: ', data)
     } catch (err) {
       console.log("Error: ", err)
--- dapp-ui/src/app/services/pizzaToken.service.ts
+++ dapp-ui/src/app/services/pizzaToken.service.ts
@@ -9,21 +9,22 @@ import { GlobalService } from './global.service';
 export class PizzaTokenService {
   provider: ethers.providers.Provider
   erc721Address: string
+  contract: ethers.Contract
 
   constructor(private globalService: GlobalService) {
     this.provider = ethers.getDefaultProvider("ropsten", {})
     this.erc721Address = "0x0012451094E9C170E5151e0a793D716ba3915e60"
+    this.contract = new ethers.Contract(this.erc721Address, PizzaToken.abi, this.provider)
   }
 
   async buyPizza(dough: string, topping: string, extra: string) {
     const signer = await this.globalService.getSigner().toPromise() as ethers.Signer
-    let contract = new ethers.Contract(this.erc721Address, PizzaToken.abi, this.provider)
 
-    contract = await contract.connect(signer)
+    this.contract = await this.contract.connect(signer)
     try {
       const pizza = { dough, topping, extra }
       const base64Pizza = btoa(JSON.stringify(pizza))
-      const data = await contract.safeMint(await signer.getAddress(), base64Pizza)
+      const data = await this.contract.safeMint(await signer.getAddress(), base64Pizza)
       console.log('data: ', data)
     } catch (err) {
       console.log("Error: ", err)
