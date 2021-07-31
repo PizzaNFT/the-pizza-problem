93c1758b20cbae8c6e52cacf29d46d76008a4c11
denis.policastro <denis.policastro@gmail.com>
2021-07-31 18:51:39 -0300

    feat: add deploy script

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,8 +1,24 @@
 import "@nomiclabs/hardhat-waffle";
 
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "KEY";
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const ROPSTEN_PRIVATE_KEY = "";
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.3",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`],
+    },
+  }
 };
---
+++ contract/scripts/deploy.ts
@@ -0,0 +1,89 @@
+import * as dotenv from "dotenv";
+import { ethers } from "ethers";
+
+import * as PizzaBallot from "../artifacts/contracts/PizzaBallot.sol/PizzaBallot.json";
+import * as PizzaTokenJson from "../artifacts/contracts/PizzaToken.sol/PizzaToken.json";
+import * as PizzaCoinJson from "../artifacts/contracts/PizzaCoin.sol/PizzaCoin.json";
+
+dotenv.config();
+
+const PROPOSALS = ["Cheeseeeeee", "Bacon", "4 Bacon"];
+
+async function main() {
+
+  const pkey = process.env.ADMIN_PRIVATE_KEY ?? "";
+  if (pkey.length < 64) throw new Error("Private Key not set up");
+  const provider = new ethers.providers.JsonRpcProvider(
+    process.env.PROVIDER_URL
+  );
+
+  const signer = new ethers.Wallet(pkey, provider);
+
+  const pizzaCoinAddress = await deployToken(signer)
+
+  await deployBallot(signer, pizzaCoinAddress)
+
+}
+
+const deployToken = async (signer: ethers.Signer) => {
+  console.log("[PizzaToken] Deploying...")
+  const contractFactory = new ethers.ContractFactory(
+    PizzaTokenJson.abi,
+    PizzaTokenJson.bytecode,
+    signer
+  );
+  const pizzaToken = await contractFactory.deploy();
+  console.log("[PizzaToken] Awaiting confirmations");
+
+  await pizzaToken.deployed();
+  console.log("[PizzaToken] Contract deployed at: " + pizzaToken.address);
+  console.log("[PizzaToken] Deployed")
+
+  const pizzaCoinAddress = await pizzaToken.pizzaCoinAddr()
+  console.log("[PizzaToken] PizzaCoin addr: " + pizzaCoinAddress);
+
+  const minters = [
+    { role: "0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6", address: "0x2303F6e666cEa0b2bdFc41976e91D7526C6cBe0b" }
+  ]
+
+  console.log("[PizzaToken] Grating roles...")
+  minters.forEach(async minter => {
+    await pizzaToken.grantRole(minter.role, minter.address)
+  })
+
+  // Testing
+  // minters.forEach(async minter => {
+  //   const data = await pizzaToken.hasRole(minter.role, minter.address)
+  //   console.log("Has Role:", data)
+  // })
+
+  return pizzaCoinAddress
+}
+
+const deployBallot = async (signer: ethers.Signer, pizzaCoinAddress: string) => {
+  console.log("[PizzaBallot] Deploying...")
+  const contractFactory = new ethers.ContractFactory(
+    PizzaBallot.abi,
+    PizzaBallot.bytecode,
+    signer
+  );
+
+  let proposalsArray: string[] = [];
+  PROPOSALS.forEach((element) => {
+    proposalsArray.push(ethers.utils.formatBytes32String(element));
+  });
+  const votingDuration = 3; // days
+  const ballotContract = await contractFactory.deploy(proposalsArray, pizzaCoinAddress, votingDuration);
+  console.log("[PizzaBallot] Awaiting confirmations");
+
+  await ballotContract.deployed();
+  console.log("[PizzaBallot] Contract deployed at: " + ballotContract.address);
+  console.log("[PizzaBallot] Deployed")
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
